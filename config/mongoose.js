67b70052c3ecb5f128f3fc56ab20cd6f9dab91ac
Your Name <akashmondkar03@gmail.com>
2026-05-30 19:30:35 +0530

    done

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
 
-
-const connectDB = async()=>{
+const connectDB = async () => {
     try {
-        await mongoose.connect()
+        console.log("MONGO_URI:", process.env.MONGO_URI);
+
+        await mongoose.connect(process.env.MONGO_URI);
+
+        console.log("MongoDB Connected");
     } catch (error) {
-
-
-        
+        console.log(" MongoDB Error:");
+        console.log(error.message);
+        process.exit(1);
     }
-}
+};
+
+export default connectDB;
--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-import express from 'express'
-import mongoose from 'mongoose';
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/mongoose.js";
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("Home Page");
+});
 
-app.get('/',(req,resp)=>{
-    resp.send("home")
-})
+const PORT = process.env.PORT || 3500;
 
-app.listen(3500)
+app.listen(PORT, () => {
+    console.log(` Server running on port ${PORT}`);
+});
